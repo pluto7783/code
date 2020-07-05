9ac8060b4fc1c2cb409bf61eab30aa93cc9a95c8
DESKTOP-VU8J26H\kjc <karolus@naver.com>
2020-07-06 04:01:36 +0900

    first commit

---
+++ WebContent/WEB-INF/jsp/home2.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	main controller
+</h1>
+
+<P>  The time on the server is22 ${str}. </P>
+</body>
+</html>
---
+++ src/main/java/com/code/main/MainController.java
@@ -0,0 +1,56 @@
+package com.code.main;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping(value="/main")
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home2";
+	}
+	
+	@RequestMapping(value = "/index2", method = RequestMethod.GET)
+	public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home2");
+		
+		
+		
+		mv.addObject("str", "쿠쿠쿠쿠쿸쿠키키킼키키");
+		
+		return mv;
+	}
+	
+}
